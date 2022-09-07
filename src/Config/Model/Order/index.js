b8e6c63b9337f9e81378d67b22cfe83574ce191d
ifaizinur <inurfaizi@gmail.comm>
2022-09-07 16:19:34 +0700

    minor update, model order params date

--- src/Config/Model/Order/index.js
+++ src/Config/Model/Order/index.js
@@ -5,7 +5,7 @@ const getOrders = async (merchantId, page = null) => {
     try {
         let orderData = await POST(`${merchantId}/order/all` + `${page != null ? `?page=${page}&limit=5` : ''}`, {
             createdAt: {
-                start: __DEV__ ? `2020-08-08 00:00:00` : `${moment().format('YYYY-MM-DD')} 00:00:00`,
+                start: `2020-08-08 00:00:00`,
                 end: `${moment().format('YYYY-MM-DD HH:mm:ss')}`,
             }
         });
